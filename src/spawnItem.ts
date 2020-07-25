948ce435774fa76f3972be230140fb3086693fd3
Tak-John Cheung <tak_john@hotmail.com>
2020-07-25 13:37:57 +0100

    added more comments

--- src/spawnItem.ts
+++ src/spawnItem.ts
@@ -21,21 +21,18 @@ export class SpawnItem extends Entity {
     this.addComponent(
       new utils.TriggerComponent(
         new utils.TriggerBoxShape(new Vector3(1.5, 3, 1.5), Vector3.Zero()), // We need a separate trigger instance for each item as we'll be modifying it
-        0,
-        0,
-        null,
-        null,
+        0, 0, null,null, // Default params for trigger
+        // Camera enter trigger
         () => {
-          log("item collected")
           this.soundEntity.getComponent(AudioSource).playOnce()
           this.getComponent(Transform).scale.setAll(0)
-          let origTriggerPos = this.getComponent(utils.TriggerComponent).shape.position.y
+          let origTriggerPosY = this.getComponent(utils.TriggerComponent).shape.position.y
           this.getComponent(utils.TriggerComponent).shape.position.y = -100 // Move the trigger so that the player exits and re-enters the trigger
 
           this.addComponent(
             new utils.Delay(respawnTime, () => {
               this.getComponent(Transform).scale.setAll(1)
-              this.getComponent(utils.TriggerComponent).shape.position.y = origTriggerPos
+              this.getComponent(utils.TriggerComponent).shape.position.y = origTriggerPosY // Revert trigger position back to its original position
             })
           )
         }
