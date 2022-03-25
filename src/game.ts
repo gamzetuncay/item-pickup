e45f5c51236ce3dd8fe049aaeae0a2df6dd19b98
nearnshaw <nearnshaw@decentraland.org>
2022-03-25 13:44:52 -0300

    use function, not object

--- src/game.ts
+++ src/game.ts
@@ -7,28 +7,28 @@ base.addComponent(new GLTFShape('models/baseLight.glb'))
 engine.addEntity(base)
 
 // Items
-const health = new SpawnItem(
+const health = SpawnItem(
   new GLTFShape('models/medikit.glb'),
   new Transform({
-    position: new Vector3(4, 0.75, 6)
+    position: new Vector3(4, 0.75, 6),
   }),
   new AudioClip('sounds/medikitPickup.mp3'),
   3000 // In milliseconds - 3 second spawn time
 )
 
-const ammo = new SpawnItem(
+const ammo = SpawnItem(
   new GLTFShape('models/ammo.glb'),
   new Transform({
-    position: new Vector3(8, 0.75, 10)
+    position: new Vector3(8, 0.75, 10),
   }),
   new AudioClip('sounds/ammoPickup.mp3'),
   1500
 )
 
-const armor = new SpawnItem(
+const armor = SpawnItem(
   new GLTFShape('models/armor.glb'),
   new Transform({
-    position: new Vector3(12, 0.75, 6)
+    position: new Vector3(12, 0.75, 6),
   }),
   new AudioClip('sounds/armorPickup.mp3'),
   5000
@@ -38,20 +38,20 @@ const armor = new SpawnItem(
 const spawnBaseRed = new SpawnBase(
   new GLTFShape('models/spawnBaseRed.glb'),
   new Transform({
-    position: new Vector3(4, 0, 6)
+    position: new Vector3(4, 0, 6),
   })
 )
 
 const spawnBaseGreen = new SpawnBase(
   new GLTFShape('models/spawnBaseGreen.glb'),
   new Transform({
-    position: new Vector3(8, 0, 10)
+    position: new Vector3(8, 0, 10),
   })
 )
 
 const spawnBaseBlue = new SpawnBase(
   new GLTFShape('models/spawnBaseBlue.glb'),
   new Transform({
-    position: new Vector3(12, 0, 6)
+    position: new Vector3(12, 0, 6),
   })
 )
--- src/spawnItem.ts
+++ src/spawnItem.ts
@@ -1,49 +1,49 @@
 import * as utils from '@dcl/ecs-scene-utils'
 
-export class SpawnItem extends Entity {
-  private soundEntity = new Entity()
+export function SpawnItem(
+  model: GLTFShape,
+  transform: Transform,
+  sound: AudioClip,
+  respawnTime: number
+) {
+  let entity = new Entity()
+  engine.addEntity(entity)
+  entity.addComponent(model)
+  entity.addComponent(transform)
 
-  constructor(
-    model: GLTFShape,
-    transform: Transform,
-    sound: AudioClip,
-    respawnTime: number
-  ) {
-    super()
-    engine.addEntity(this)
-    this.addComponent(model)
-    this.addComponent(transform)
+  let soundEntity = new Entity()
+  soundEntity.addComponent(new AudioSource(sound))
+  soundEntity.addComponent(new Transform())
 
-    this.soundEntity.addComponent(new AudioSource(sound))
-    this.soundEntity.addComponent(new Transform())
-    this.soundEntity.setParent(Attachable.AVATAR)
-    engine.addEntity(this.soundEntity)
+  engine.addEntity(soundEntity)
+  soundEntity.setParent(Attachable.AVATAR)
 
-    /**
-     * This trigger allows the player to stand on the same spot and continually
-     * pick up an item without having to exit and re-enter the trigger themselves
-     */
-    this.addComponent(
-      new utils.TriggerComponent(
-        new utils.TriggerBoxShape(new Vector3(1.5, 3, 1.5)), // We need a separate trigger instance for each item as we'll be modifying it
-        {
-          onCameraEnter: () => {
-            this.soundEntity.getComponent(AudioSource).playOnce()
-            this.getComponent(Transform).scale.setAll(0)
-            const origTriggerPosY = this.getComponent(utils.TriggerComponent)
-              .shape.position.y
-            this.getComponent(utils.TriggerComponent).shape.position.y = -100 // Move the trigger so that the player exits and re-enters the trigger
+  /**
+   * This trigger allows the player to stand on the same spot and continually
+   * pick up an item without having to exit and re-enter the trigger themselves
+   */
+  entity.addComponent(
+    new utils.TriggerComponent(
+      new utils.TriggerBoxShape(new Vector3(1.5, 3, 1.5)), // We need a separate trigger instance for each item as we'll be modifying it
+      {
+        onCameraEnter: () => {
+          soundEntity.getComponent(AudioSource).playOnce()
+          entity.getComponent(Transform).scale.setAll(0)
+          const origTriggerPosY = entity.getComponent(utils.TriggerComponent)
+            .shape.position.y
+          entity.getComponent(utils.TriggerComponent).shape.position.y = -100 // Move the trigger so that the player exits and re-enters the trigger
 
-            this.addComponent(
-              new utils.Delay(respawnTime, () => {
-                this.getComponent(Transform).scale.setAll(1)
-                this.getComponent(utils.TriggerComponent).shape.position.y =
-                  origTriggerPosY // Revert trigger position back to its original position
-              })
-            )
-          }
-        }
-      )
+          entity.addComponent(
+            new utils.Delay(respawnTime, () => {
+              entity.getComponent(Transform).scale.setAll(1)
+              entity.getComponent(utils.TriggerComponent).shape.position.y =
+                origTriggerPosY // Revert trigger position back to its original position
+            })
+          )
+        },
+      }
     )
-  }
+  )
+
+  return entity
 }
