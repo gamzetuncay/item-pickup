be3336c10d39498b979a23f81c4ca03d63f54825
digimindmarket <gamzetuncay@outlook.com>
2022-12-28 17:34:39 +0300

    sample scene

--- src/game.ts
+++ src/game.ts
@@ -1,37 +1,75 @@
 import { SpawnItem } from './spawnItem'
 import { SpawnBase } from './spawnBase'
 
+
 // Base scene
 const base = new Entity()
 base.addComponent(new GLTFShape('models/baseLight.glb'))
 engine.addEntity(base)
 
+// Trees
+const tree = new Entity()
+tree.addComponent(new GLTFShape('models/TreeSycamore_03.glb'))
+tree.addComponent(new Transform({position: new Vector3(4, 0, 14),scale: new Vector3(1, 1, 1)}),)
+tree.setParent(base)
+engine.addEntity(tree)
+
+const tree2 = new Entity()
+tree2.addComponent(new GLTFShape('models/TreeFir_02.glb'))
+tree2.addComponent(new Transform({position: new Vector3(8, 0, 14),scale: new Vector3(1, 1, 1)}),)
+tree2.setParent(base)
+engine.addEntity(tree2)
+
+const tree3 = new Entity()
+tree3.addComponent(new GLTFShape('models/TreePine_01.glb'))
+tree3.addComponent(new Transform({position: new Vector3(12, 0, 14),scale: new Vector3(1, 1, 1)}),)
+tree3.setParent(base)
+engine.addEntity(tree3)
+
+// Sustainable Objects
+const solarPanel = new Entity()
+solarPanel.addComponent(new GLTFShape('models/SolarPanel_01.glb'))
+solarPanel.addComponent(new Transform({position: new Vector3(4, 0.2, 8),scale: new Vector3(1, 1, 1),rotation: Quaternion.Euler(0, -180, 0)}),)
+solarPanel.setParent(base) 
+engine.addEntity(solarPanel)
+
+const flower = new Entity()
+flower.addComponent(new GLTFShape('models/Flower_03.glb'))
+flower.addComponent(new Transform({position: new Vector3(8, 0.2, 8), scale: new Vector3(10, 10, 10)}),)
+flower.setParent(base)
+engine.addEntity(flower)
+
+const bicycle = new Entity()
+bicycle.addComponent(new GLTFShape('models/Bicycle_01.glb'))
+bicycle.addComponent(new Transform({position: new Vector3(12, 0.2, 8),scale: new Vector3(1, 1, 1)}),)
+bicycle.setParent(base)
+engine.addEntity(bicycle)
+
+
+
 // Items
 const health = SpawnItem(
-  new GLTFShape('models/medikit.glb'),
-  new Transform({
-    position: new Vector3(4, 0.75, 6),
-  }),
+  new GLTFShape('models/bottle_anim.glb'),
+  solarPanel,
+  new Transform({position: new Vector3(4, 0.75, 6),}),
   new AudioClip('sounds/medikitPickup.mp3'),
-  3000 // In milliseconds - 3 second spawn time
+  1 // In milliseconds - 3 second spawn time
 )
 
 const ammo = SpawnItem(
-  new GLTFShape('models/ammo.glb'),
-  new Transform({
-    position: new Vector3(8, 0.75, 10),
-  }),
+  new GLTFShape('models/bottle_anim.glb'),
+  flower,
+  new Transform({position: new Vector3(8, 0.75, 6),}),
   new AudioClip('sounds/ammoPickup.mp3'),
-  1500
+  1
 )
 
 const armor = SpawnItem(
-  new GLTFShape('models/armor.glb'),
-  new Transform({
-    position: new Vector3(12, 0.75, 6),
-  }),
+  new GLTFShape('models/bottle_anim.glb'),
+  bicycle,
+  new Transform({position: new Vector3(12, 0.75, 6),}),
   new AudioClip('sounds/armorPickup.mp3'),
-  5000
+  1
 )
 
 // Spawn Bases
@@ -45,7 +83,7 @@ const spawnBaseRed = new SpawnBase(
 const spawnBaseGreen = new SpawnBase(
   new GLTFShape('models/spawnBaseGreen.glb'),
   new Transform({
-    position: new Vector3(8, 0, 10),
+    position: new Vector3(8, 0, 6),
   })
 )
 
--- src/spawnItem.ts
+++ src/spawnItem.ts
@@ -1,43 +1,43 @@
 import * as utils from '@dcl/ecs-scene-utils'
 
-export function SpawnItem(
-  model: GLTFShape,
-  transform: Transform,
-  sound: AudioClip,
-  respawnTime: number
-) {
-  let entity = new Entity()
-  engine.addEntity(entity)
-  entity.addComponent(model)
-  entity.addComponent(transform)
+export function SpawnItem(pickable_entity_model: GLTFShape,
+                          spawn_entity: Entity,
+                          transform1: Transform,
+                          sound: AudioClip,
+                          respawnTime: number) {
+
+  let pickable_entity = new Entity()
+  engine.addEntity(pickable_entity)
+  pickable_entity.addComponent(pickable_entity_model)
+  pickable_entity.addComponent(transform1)
 
   let soundEntity = new Entity()
   soundEntity.addComponent(new AudioSource(sound))
   soundEntity.addComponent(new Transform())
-
   engine.addEntity(soundEntity)
   soundEntity.setParent(Attachable.AVATAR)
+                          
+  spawn_entity.getComponent(Transform).scale.setAll(0)
+  
+
 
   /**
    * This trigger allows the player to stand on the same spot and continually
    * pick up an item without having to exit and re-enter the trigger themselves
    */
-  entity.addComponent(
+  pickable_entity.addComponent(
     new utils.TriggerComponent(
       new utils.TriggerBoxShape(new Vector3(1.5, 3, 1.5)), // We need a separate trigger instance for each item as we'll be modifying it
       {
         onCameraEnter: () => {
           soundEntity.getComponent(AudioSource).playOnce()
-          entity.getComponent(Transform).scale.setAll(0)
-          const origTriggerPosY = entity.getComponent(utils.TriggerComponent)
-            .shape.position.y
-          entity.getComponent(utils.TriggerComponent).shape.position.y = -100 // Move the trigger so that the player exits and re-enters the trigger
-
-          entity.addComponent(
+          pickable_entity.getComponent(Transform).scale.setAll(0)
+          //const origTriggerPosY = pickable_entity.getComponent(utils.TriggerComponent).shape.position.y
+          pickable_entity.getComponent(utils.TriggerComponent).shape.position.y = -100 // Move the trigger so that the player exits and re-enters the trigger
+          
+          spawn_entity.addComponent(
             new utils.Delay(respawnTime, () => {
-              entity.getComponent(Transform).scale.setAll(1)
-              entity.getComponent(utils.TriggerComponent).shape.position.y =
-                origTriggerPosY // Revert trigger position back to its original position
+              spawn_entity.getComponent(Transform).scale.setAll(1)
             })
           )
         },
@@ -45,5 +45,5 @@ export function SpawnItem(
     )
   )
 
-  return entity
+  return pickable_entity
 }
